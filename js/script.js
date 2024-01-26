f4d4e97baec9a5ba9981fb14774e164a549b648c
Javier Blasco <blascojavi@gmail.com>
2024-01-26 16:48:02 +0100

    button

--- js/script.js
+++ js/script.js
@@ -10,22 +10,22 @@ let todoList = [];
 let userIsEditing = null;
 
 function resetTodoList() {
-    todoUlList.innerHTML = ''
+    todoUlList.innerHTML = '';
     btnAddEditTodo.innerText = "Agregar";
 }
 
 const createTodoButton = (text, id, onClick) => {
     const btnEditTodo = document.createElement("button");
     btnEditTodo.innerText = text;
-    btnEditTodo.onclick = () => onClick(id)
-    return btnEditTodo
+    btnEditTodo.onclick = () => onClick(id);
+    return btnEditTodo;
 }
-
 
 const onClickDeleteTodo = (id) => {
     const newTodoList = todoList.filter(todo => todo.id !== id);
-    todoList = newTodoList
-    renderTodos()
+    todoList = newTodoList;
+    renderTodos();
+    updateTodoQuantity(); // Actualizar la cantidad de tareas
 }
 
 const onClickEditButton = (id) => {
@@ -37,62 +37,67 @@ const onClickEditButton = (id) => {
 
 const editTodo = () => {
     const todoToEdit = todoList.find(todo => todo.id === userIsEditing);
-    todoToEdit.text = todoInputText.value
-    userIsEditing = null
-    renderTodos()
-    todoInputText.value =null
+    todoToEdit.text = todoInputText.value;
+    userIsEditing = null;
+    renderTodos();
+    updateTodoQuantity(); // Actualizar la cantidad de tareas
+    todoInputText.value = null;
 }
 
 function renderTodos() {
-    resetTodoList()
+    resetTodoList();
     const AllTodos = todoList.map(todo => {
         const li = document.createElement("li");
         li.textContent = todo.text;
         li.id = todo.id;
-        const editButton = createTodoButton("Editar", todo.id, onClickEditButton)
-        const deleteButton = createTodoButton("Borrar", todo.id, onClickDeleteTodo)
+        const editButton = createTodoButton("Editar", todo.id, onClickEditButton);
+        const deleteButton = createTodoButton("Borrar", todo.id, onClickDeleteTodo);
         li.appendChild(editButton);
         li.appendChild(deleteButton);
         return li;
-       
-    })
+    });
     AllTodos.forEach(todo => {
         todoUlList.appendChild(todo);
-    })  
+    });
 }
 
 function addTodo() {
     if (todoInputText.value == "") {
         return;
     }
-   const newTodoText = todoInputText.value;
-   const newTodoId = `${new Date().getTime()}${Math.random()}`;
-   const newTodo = {
-    text: newTodoText,
-    id: newTodoId
-   }
-   todoList.push(newTodo)
-   renderTodos()
-   todoInputText.value =null
+    const newTodoText = todoInputText.value;
+    const newTodoId = `${new Date().getTime()}${Math.random()}`;
+    const newTodo = {
+        text: newTodoText,
+        id: newTodoId
+    };
+    todoList.push(newTodo);
+    renderTodos();
+    updateTodoQuantity(); // Actualizar la cantidad de tareas
+    todoInputText.value = null;
 }
 
 const onClickBtnAddEditTodo = () => {
-        if (userIsEditing) {
-            return editTodo()
-        }
-        if (!userIsEditing) {
-            return addTodo()
-        }
+    if (userIsEditing) {
+        return editTodo();
+    }
+    if (!userIsEditing) {
+        return addTodo();
+    }
 }
 
 btnAddEditTodo.onclick = onClickBtnAddEditTodo;
 
 todoInputText.addEventListener("keypress", function (event) {
-    if (btnAddEditTodo.innerText == "Modificar"){
-        console.log(btnAddEditTodo.innerText)
-        return
+    if (btnAddEditTodo.innerText == "Modificar") {
+        console.log(btnAddEditTodo.innerText);
+        return;
     }
     if (event.key === "Enter") {
         addTodo();
     }
 });
+
+function updateTodoQuantity() {
+    todoQuantity.innerText = todoList.length;
+}
